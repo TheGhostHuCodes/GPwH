76775772032c8af41bf21eade953487820ec094b
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2018-06-30 07:48:43 -0700

    Refactor letter drafting program into smaller functions

--- Ch01/first_prog.hs
+++ Ch01/first_prog.hs
@@ -1,11 +1,17 @@
-messyMain :: IO()
-messyMain = do
+main :: IO()
+main = do
     print "Who is the email for?"
     recipient <- getLine
     print "What is the Title?"
     title <- getLine
     print "Who is the Author?"
     author <- getLine
-    print ("Dear " ++ recipient ++ ",\n" ++
-        "Thanks for buying " ++ title ++ "\nthanks,\n" ++
-        author)
+    print (createEmail recipient title author)
+
+
+toPart recipient = "Dear " ++ recipient ++ ",\n"
+bodyPart bookTitle = "Thanks for buying " ++ bookTitle ++ ".\n"
+fromPart author = "Thanks,\n" ++ author
+createEmail recipient bookTitle author = toPart recipient ++
+                                         bodyPart bookTitle ++
+                                         fromPart author
