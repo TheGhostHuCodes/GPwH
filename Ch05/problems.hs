32307dbeb2f9ce257cdfff10144207ee3ab760f5
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2018-07-01 21:52:04 -0700

    Add solutions to chapter 5 problems

---
+++ Ch05/problems.hs
@@ -0,0 +1,15 @@
+-- Question 5.1
+inc n = n + 1
+double n = n * 2
+square n = n ^ 2
+
+ifEven fn n = if even n
+              then fn n
+              else n
+
+ifEvenInc = ifEven inc
+ifEvenDouble = ifEven double
+ifEvenSquare = ifEven square
+
+-- Question 5.2
+binaryPartialApplication fn x = (\y -> fn x y)
