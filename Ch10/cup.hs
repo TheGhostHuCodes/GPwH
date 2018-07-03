4c759bbb6f5af62aeda278a2f2facdf722b17a6c
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2018-07-03 12:40:23 -0700

    Add cup code listing from chapter 10

---
+++ Ch10/cup.hs
@@ -0,0 +1,13 @@
+cup flOz = \message -> message flOz
+
+coffeeCup = cup 12
+
+getOz aCup = aCup (\flOz -> flOz)
+drink aCup ozDrank = if ozDiff >= 0
+                     then cup ozDiff
+                     else cup 0
+    where flOz = getOz aCup
+          ozDiff = flOz - ozDrank
+isEmpty aCup = getOz aCup == 0
+
+afterManySips = foldl drink coffeeCup [1,1,1,1,1]
