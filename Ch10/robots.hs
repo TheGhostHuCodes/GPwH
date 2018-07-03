4fb00fa9951d05f7428db68893c7ff8d8f5f0821
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2018-07-03 12:41:27 -0700

    Add robots code listing from chapter 10

---
+++ Ch10/robots.hs
@@ -0,0 +1,45 @@
+robot (name, attack, hp) = \message -> message (name, attack, hp)
+name (n, _, _) = n
+attack (_, a, _) = a
+hp (_, _, hp) = hp
+
+getName aRobot = aRobot name
+getAttack aRobot = aRobot attack
+getHP aRobot = aRobot hp
+
+setName aRobot newName = aRobot (\(n, a, h) -> robot (newName, a, h))
+setAttack aRobot newAttack = aRobot (\(n, a, h) -> robot (n, newAttack, h))
+setHP aRobot newHP = aRobot (\(n, a, h) -> robot (n, a, newHP))
+
+printRobot aRobot = aRobot (\(n, a, h) -> n
+                                          ++ " attack:" ++ (show a)
+                                          ++ " hp:" ++ (show h))
+
+damage aRobot attackDamage = aRobot (\(n, a, h) -> robot (n, a, h - attackDamage))
+
+fight aRobot defender = damage defender attack
+    where attack = if getHP aRobot > 0
+                   then getAttack aRobot
+                   else 0
+
+killerRobot = robot ("Kill3r", 25, 200)
+nicerRobot = setName killerRobot "kitty"
+gentlerRobot = setAttack killerRobot 5
+softerRobot = setHP killerRobot 50
+gentleGiant = robot ("Mr. Friendly", 10, 300)
+
+gentleGiantRound1 = fight killerRobot gentleGiant
+killerRobotRound1 = fight gentleGiant killerRobot
+gentleGiantRound2 = fight killerRobotRound1 gentleGiantRound1
+killerRobotRound2 = fight gentleGiantRound1 killerRobotRound1
+gentleGiantRound3 = fight killerRobotRound2 gentleGiantRound2
+killerRobotRound3 = fight gentleGiantRound2 killerRobotRound2
+
+fastRobot = robot ("speedy", 15, 40)
+slowRobot = robot ("slowpoke",20, 30)
+
+slowRobotRound1 = fight fastRobot slowRobot
+fastRobotRound1 = fight slowRobotRound1 fastRobot
+slowRobotRound2 = fight fastRobotRound1 slowRobotRound1
+slowRobotRound3 = fight fastRobotRound1 slowRobotRound2
+fastRobotRound2 = fight slowRobotRound3 fastRobotRound1
