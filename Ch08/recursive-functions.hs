a71ce38c3785018d3341717defc238c7d69612f7
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2018-07-02 21:47:01 -0700

    Add code listings from chapter 8

---
+++ Ch08/recursive-functions.hs
@@ -0,0 +1,18 @@
+myLength [] = 0
+myLength (_:xs) = 1 + myLength xs
+
+myTake _ [] = []
+myTake 0 _ = []
+myTake n (x:xs) = x:rest
+    where rest = myTake (n - 1) xs
+
+myCycle (x:xs) = x:myCycle (xs++[x])
+
+ackermann 0 n = n + 1
+ackermann m 0 = ackermann (m - 1) 1
+ackermann m n = ackermann (m - 1) (ackermann m (n - 1))
+
+collatz 1 = 1
+collatz n = if even n
+    then 1 + collatz (n `div` 2)
+    else 1 + collatz (n * 3 + 1)
