98b844969c1c89544ea6a776bed69de00037ea23
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2018-07-08 22:58:01 -0700

    Add solutions to chapter 16 problems

--- Ch16/product-and-sum-types.hs
+++ Ch16/product-and-sum-types.hs
@@ -38,15 +38,44 @@ data VinylRecord = VinylRecord {
 
 data CollectibleToy = CollectibleToy {
     name :: String
-  , description :: String
+  , toyDescription :: String
   , toyPrice :: Double
+}
+
+-- Question 16.1
+data Pamphlet = Pamphlet {
+    title :: String
+  , pamphletDescription :: String
+  , contact :: String
 }
 
 data StoreItem = BookItem Book 
                | RecordItem VinylRecord
                | ToyItem CollectibleToy
+               | PamphletItem Pamphlet
 
 price :: StoreItem -> Double
 price (BookItem book) = bookPrice book
 price (RecordItem record) = recordPrice record
 price (ToyItem toy) = toyPrice toy
+price (PamphletItem _) = 0.0
+
+-- Question 16.2
+type Radius = Double
+type Height = Double
+type Width = Double
+
+data Shape = Circle Radius
+           | Square Height
+           | Rectangle Height Width
+           deriving Show
+
+parimeter :: Shape -> Double
+parimeter (Circle r) = 2 * pi * r
+parimeter (Square h) = 4 * h
+parimeter (Rectangle h w) = 2 * h + 2 * w
+
+area :: Shape -> Double
+area (Circle r) = pi * r ^ 2
+area (Square h) = h ^ 2
+area (Rectangle h w) = h * w
