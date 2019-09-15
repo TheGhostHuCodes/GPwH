2dd8dd90c3b39d0b0a1d4d1b0f36819394ce0d45
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2019-09-15 10:34:11 -0700

    Fix lint errors and refactor solutions in chapter 15 code

--- Ch15/secret-messages.hs
+++ Ch15/secret-messages.hs
@@ -1,91 +1,99 @@
-import Data.Char
+import           Data.Char
 
 data FourLetterAlphabet = L1 | L2 | L3 | L4 deriving (Show, Enum, Bounded)
 
 rotN :: (Bounded a, Enum a) => Int -> a -> a
 rotN alphabetSize ch = toEnum rotation
-    where halfAlphabet = alphabetSize `div` 2
-          offset = fromEnum ch + halfAlphabet
-          rotation = offset `mod` alphabetSize
+ where
+  halfAlphabet = alphabetSize `div` 2
+  offset       = fromEnum ch + halfAlphabet
+  rotation     = offset `mod` alphabetSize
 
 message :: [FourLetterAlphabet]
 message = [L1, L3, L4, L1, L1, L2]
 
 fourLetterAlphabetEncoder :: [FourLetterAlphabet] -> [FourLetterAlphabet]
 fourLetterAlphabetEncoder vals = map rot4l vals
-    where alphaSize = 1 + fromEnum (maxBound :: FourLetterAlphabet)
-          rot4l = rotN alphaSize
+ where
+  alphaSize = 1 + fromEnum (maxBound :: FourLetterAlphabet)
+  rot4l     = rotN alphaSize
 
-data ThreeLetterAlphabet = Alpha
-                         | Beta
-                         | Kappa deriving (Show, Enum, Bounded)
+data ThreeLetterAlphabet = Alpha | Beta | Kappa deriving (Show, Enum, Bounded)
 
 threeLetterMessage :: [ThreeLetterAlphabet]
 threeLetterMessage = [Alpha, Alpha, Beta, Alpha, Kappa]
 
 threeLetterEncoder :: [ThreeLetterAlphabet] -> [ThreeLetterAlphabet]
 threeLetterEncoder vals = map rot3l vals
-    where alphaSize = 1 + fromEnum (maxBound :: ThreeLetterAlphabet)
-          rot3l = rotN alphaSize
+ where
+  alphaSize = 1 + fromEnum (maxBound :: ThreeLetterAlphabet)
+  rot3l     = rotN alphaSize
 
 rotNDecoder :: (Bounded a, Enum a) => Int -> a -> a
 rotNDecoder n ch = toEnum rotation
-    where halfN = n `div` 2
-          offset = if even n
-                  then fromEnum ch + halfN
-                  else 1 + fromEnum ch + halfN
-          rotation = offset `mod` n
+ where
+  halfN    = n `div` 2
+  offset   = if even n then fromEnum ch + halfN else 1 + fromEnum ch + halfN
+  rotation = offset `mod` n
 
 threeLetterDecoder :: [ThreeLetterAlphabet] -> [ThreeLetterAlphabet]
 threeLetterDecoder vals = map rot3ldecoder vals
-    where alphaSize = 1 + fromEnum (maxBound :: ThreeLetterAlphabet)
-          rot3ldecoder = rotNDecoder alphaSize
+ where
+  alphaSize    = 1 + fromEnum (maxBound :: ThreeLetterAlphabet)
+  rot3ldecoder = rotNDecoder alphaSize
 
 rotEncoder :: String -> String
 rotEncoder text = map rotChar text
-    where alphaSize = 1 + fromEnum (maxBound :: Char)
-          rotChar = rotN alphaSize
+ where
+  alphaSize = 1 + fromEnum (maxBound :: Char)
+  rotChar   = rotN alphaSize
 
 rotDecoder :: String -> String
 rotDecoder text = map rotCharDecoder text
-    where alphaSize = 1 + fromEnum (maxBound :: Char)
-          rotCharDecoder = rotNDecoder alphaSize
+ where
+  alphaSize      = 1 + fromEnum (maxBound :: Char)
+  rotCharDecoder = rotNDecoder alphaSize
 
 xorBool :: Bool -> Bool -> Bool
-xorBool v1 v2 = (v1 || v2) && (not (v1 && v2))
+xorBool v1 v2 = (v1 || v2) && not (v1 && v2)
+
 xorPair :: (Bool, Bool) -> Bool
 xorPair (v1, v2) = xorBool v1 v2
+
 xor :: [Bool] -> [Bool] -> [Bool]
 xor l1 l2 = map xorPair (zip l1 l2)
 
 type Bits = [Bool]
 
 intToBits' :: Int -> Bits
-intToBits' 0  = [False]
+intToBits' 0 = [False]
 intToBits' 1 = [True]
-intToBits' n = if (remainder == 0)
-               then False : intToBits' nextVal
-               else True : intToBits' nextVal
-    where remainder = n `mod` 2
-          nextVal = n `div` 2
+intToBits' n = if remainder == 0
+  then False : intToBits' nextVal
+  else True : intToBits' nextVal
+ where
+  remainder = n `mod` 2
+  nextVal   = n `div` 2
 
 maxBits :: Int
 maxBits = length (intToBits' maxBound)
 
 intToBits :: Int -> Bits
 intToBits n = leadingFalses ++ reversedBits
-    where reversedBits = reverse (intToBits' n)
-          missingBits = maxBits - (length reversedBits)
-          leadingFalses = take missingBits (cycle [False])
+ where
+  reversedBits  = reverse (intToBits' n)
+  missingBits   = maxBits - length reversedBits
+  leadingFalses = take missingBits (cycle [False])
 
 charToBits :: Char -> Bits
 charToBits char = intToBits (fromEnum char)
 
 bitsToInt :: Bits -> Int
-bitsToInt bits = sum (map (\x -> 2 ^ (snd x)) trueLocations)
-    where size = length bits
-          indices = [size - 1, size - 2 .. 0]
-          trueLocations = filter (\x -> fst x == True) (zip bits indices)
+bitsToInt bits = sum (map (\x -> 2 ^ snd x) trueLocations)
+ where
+  size          = length bits
+  indices       = [size - 1, size - 2 .. 0]
+  trueLocations = filter (\x -> fst x == True) (zip bits indices)
 
 bitsToChar :: Bits -> Char
 bitsToChar bits = toEnum (bitsToInt bits)
@@ -97,15 +105,15 @@ myPlainText :: String
 myPlainText = "Haskell"
 
 applyOTP' :: String -> String -> [Bits]
-applyOTP' pad plaintext = map (\pair ->
-                                (fst pair) `xor` (snd pair))
-                          (zip padBits plaintextBits)
-    where padBits = map charToBits pad
-          plaintextBits = map charToBits plaintext
+applyOTP' pad plaintext = map (\pair -> fst pair `xor` snd pair)
+                              (zip padBits plaintextBits)
+ where
+  padBits       = map charToBits pad
+  plaintextBits = map charToBits plaintext
 
 applyOTP :: String -> String -> String
 applyOTP pad plaintext = map bitsToChar bitList
-    where bitList = applyOTP' pad plaintext
+  where bitList = applyOTP' pad plaintext
 
 encoderDecoder :: String -> String
 encoderDecoder = applyOTP myPad
@@ -141,25 +149,24 @@ myPRNG = prng 1337 7 (ord (maxBound :: Char))
 randList :: Int -> Int -> [Int]
 randList seed 0 = []
 randList seed n = randNum : randList randNum (n - 1)
-    where randNum = myPRNG seed
+  where randNum = myPRNG seed
 
-data StreamCipher = SC Int
+newtype StreamCipher = SC Int
 
 instance Cipher StreamCipher where
-    encode (SC seed) text = applySC seed text
-    decode (SC seed) text = applySC seed text
+    encode (SC seed) = applySC seed
+    decode (SC seed) = applySC seed
 
 mySC :: StreamCipher
 mySC = SC 42
 
 applySC' :: Int -> String -> [Bits]
-applySC' seed plaintext = map (\pair ->
-                                (fst pair) `xor` (snd pair))
-                          (zip randPadBits plaintextBits)
-    where randPad = randList seed (length plaintext)
-          randPadBits = map intToBits randPad
-          plaintextBits = map charToBits plaintext
+applySC' seed plaintext = zipWith xor randPadBits plaintextBits
+ where
+  randPad       = randList seed (length plaintext)
+  randPadBits   = map intToBits randPad
+  plaintextBits = map charToBits plaintext
 
 applySC :: Int -> String -> String
 applySC seed plaintext = map bitsToChar bitList
-    where bitList = applySC' seed plaintext
+  where bitList = applySC' seed plaintext
