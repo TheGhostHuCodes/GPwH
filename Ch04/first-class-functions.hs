e3d94bbbc233be9a8fc490397e886ca430863396
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2019-09-15 10:34:11 -0700

    Fix lint errors in chapter 4 problems

--- Ch04/first-class-functions.hs
+++ Ch04/first-class-functions.hs
@@ -1,9 +1,7 @@
-import Data.List
+import           Data.List
 
 
-ifEven myFunction x = if even x
-                      then myFunction x
-                      else x                
+ifEven myFunction x = if even x then myFunction x else x
 
 inc n = n + 1
 double n = n * 2
@@ -11,47 +9,46 @@ square n = n ^ 2
 
 ifEvenInc n = ifEven inc n
 ifEvenDouble n = ifEven double n
-ifEvenSquare n = ifEven square n 
+ifEvenSquare n = ifEven square n
 
-names = [("Ian", "Curtis"),
-         ("Bernard","Sumner"),
-         ("Peter", "Hook"),
-         ("Stephen","Morris"),
-         ("Steve","Morris")]
+names =
+  [ ("Ian"    , "Curtis")
+  , ("Bernard", "Sumner")
+  , ("Peter"  , "Hook")
+  , ("Stephen", "Morris")
+  , ("Steve"  , "Morris")
+  ]
 
 -- Question 4.1
-compareLastNames name1 name2 = if compare lastName1 lastName2 /= EQ
-                               then compare lastName1 lastName2
-                               else compare firstName1 firstName2
-    where lastName1 = snd name1
-          lastName2 = snd name2
-          firstName1 = fst name1
-          firstName2 = fst name2
+compareLastNames name1 name2 = if lastNameResult == EQ
+  then compare (fst name1) (fst name2)
+  else lastNameResult
+  where lastNameResult = compare (snd name1) (snd name2)
 
 sfOffice name = if lastName < "L"
-                then nameText
-                    ++ " - PO Box 1234 - San Francisco, CA, 94111"
-                else nameText
-                    ++ " - PO Box 1010 - San Francisco, CA, 94109"
-    where lastName = snd name
-          nameText = (fst name ++ " " ++ lastName)
+  then nameText ++ " - PO Box 1234 - San Francisco, CA, 94111"
+  else nameText ++ " - PO Box 1010 - San Francisco, CA, 94109"
+ where
+  lastName = snd name
+  nameText = fst name ++ " " ++ lastName
 
 nyOffice name = nameText ++ ": PO Box 789 - New York, NY, 10013"
-    where nameText = (fst name) ++ " " ++ (snd name)
+  where nameText = fst name ++ " " ++ snd name
 
-renoOffice name = nameText ++ " - PO Box 456 - Reno, NV 89523"    
-    where nameText = snd name
+renoOffice name = nameText ++ " - PO Box 456 - Reno, NV 89523"
+  where nameText = snd name
 
 -- Question 4.2
-dcOffice name = nameText ++ " - 1600 Pennsylvania Ave NW - Washington, DC 20500"
-    where nameText = (fst name) ++ " " ++ (snd name) ++ ", Esq."
+dcOffice name =
+  nameText ++ " - 1600 Pennsylvania Ave NW - Washington, DC 20500"
+  where nameText = fst name ++ " " ++ snd name ++ ", Esq."
 
 getLocationFunction location = case location of
-    "ny" -> nyOffice
-    "sf" -> sfOffice
-    "reno" -> renoOffice
-    "dc" -> dcOffice
-    _ -> (\name -> (fst name) ++ " " ++ (snd name))
+  "ny"   -> nyOffice
+  "sf"   -> sfOffice
+  "reno" -> renoOffice
+  "dc"   -> dcOffice
+  _      -> \name -> fst name ++ " " ++ snd name
 
 addressLetter name location = locationFunction name
-    where locationFunction = getLocationFunction location
+  where locationFunction = getLocationFunction location
