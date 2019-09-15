dfbc69299ba006326db809cc060e4d23ecfdca83
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2019-09-15 10:34:11 -0700

    Fix lint errors in chapter 6 code

--- Ch06/problems.hs
+++ Ch06/problems.hs
@@ -6,4 +6,4 @@ subseq i j list = drop i (take j list)
 
 -- Question 6.3
 inFirstHalf el list = el `elem` firstHalf
-    where firstHalf = take (length list `quot` 2) list
+  where firstHalf = take (length list `quot` 2) list
