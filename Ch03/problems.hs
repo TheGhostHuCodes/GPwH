5271d96460be4c976148f52503e6d9431b55604c
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2019-09-15 10:34:11 -0700

    Fix lint errors in chapter 3 problems

--- Ch03/problems.hs
+++ Ch03/problems.hs
@@ -1,9 +1,7 @@
 -- Question 3.1
-simple = (\x -> x)
-makeChange = (\owed given ->
-                if given - owed > 0
-                then given -owed
-                else 0)
+simple = \x -> x
+
+makeChange = \owed given -> if given - owed > 0 then given - owed else 0
 
 -- Question 3.2
-counter = (\x -> (\x -> x + 1) x + 1)
+counter x = (\x -> x + 1) ((\x -> x + 1) x)
