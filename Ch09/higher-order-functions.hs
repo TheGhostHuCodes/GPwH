985ec34384fca48f3ab69f75cb0ca9ae69493c44
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2019-09-15 10:34:11 -0700

    Fix lint errors in chapter 9 code

--- Ch09/higher-order-functions.hs
+++ Ch09/higher-order-functions.hs
@@ -1,35 +1,40 @@
-addAnA [] = []
-addAnA (x:xs) = ("a " ++ x):addAnA xs
+add3ToAll []       = []
+add3ToAll (x : xs) = x + 3 : add3ToAll xs
 
-squareAll [] = []
-squareAll (x:xs) = (x ^ 2):squareAll xs
+mul3ByAll []       = []
+mul3ByAll (x : xs) = x * 3 : mul3ByAll xs
 
-myMap f [] = []
-myMap f (x:xs) = (f x):myMap f xs
+addAnA []       = []
+addAnA (x : xs) = ("a " ++ x) : addAnA xs
+
+squareAll []       = []
+squareAll (x : xs) = (x ^ 2) : squareAll xs
+
+myMap f []       = []
+myMap f (x : xs) = f x : myMap f xs
 
 myFilter pred [] = []
-myFilter pred (x:xs) = if pred x
-                       then x:myFilter pred xs
-                       else myFilter pred xs
+myFilter pred (x : xs) =
+  if pred x then x : myFilter pred xs else myFilter pred xs
 
+-- Quick Check 9.1
 myRemove pred [] = []
-myRemove pred (x:xs) = if pred x
-                       then myRemove pred xs
-                       else x:myRemove pred xs
+myRemove pred (x : xs) =
+  if pred x then myRemove pred xs else x : myRemove pred xs
 
+-- Quick Check 9.2
 myProduct xs = foldl (*) 1 xs
 
 concatAll xs = foldl (++) "" xs
 
 sumOfSquares xs = foldl (+) 0 (map (^ 2) xs)
 
-rcons x y = y:x
+rcons x y = y : x
 myReverse xs = foldl rcons [] xs
 
-myFoldl f acc [] = acc
-myFoldl f acc (x:xs) = myFoldl f newAcc xs
-    where newAcc = f acc x
+myFoldl f acc []       = acc
+myFoldl f acc (x : xs) = myFoldl f newAcc xs where newAcc = f acc x
 
-myFoldr f init [] = init
-myFoldr f init (x:xs) = f x rightResult
-    where rightResult = myFoldr f init xs
+myFoldr f init []       = init
+myFoldr f init (x : xs) = f x rightResult
+  where rightResult = myFoldr f init xs
--- Ch09/problems.hs
+++ Ch09/problems.hs
@@ -1,14 +1,11 @@
-import Data.Char
-
+import           Data.Char
 
 -- Question 9.1
-myElem el xs = length (filteredList) /= 0
-    where filteredList = filter (== el) xs
+myElem el xs = length filteredList /= 0 where filteredList = filter (== el) xs
 
 -- Question 9.2
 isPalindrome s = chars == reverse chars
-    where chars = map toLower (filter (/= ' ') s)
+  where chars = map toLower (filter (/= ' ') s)
 
 -- Question 9.3
-harmonic n = sum (take n series)
-    where series = map (1.0 /) [1.0, 2.0 .. ]
+harmonic n = sum (take n series) where series = map (1.0 /) [1.0, 2.0 ..]
