1f89d04e46a1ab8edc17fa1c76789be36d31f9cf
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2019-09-15 10:34:11 -0700

    Fix lint errors and add some solutions for chapter 14 code

--- Ch14/problems.hs
+++ Ch14/problems.hs
@@ -1,5 +1,14 @@
+-- Question 14.1
+data Number = One | Two | Three deriving Enum
+
+instance Eq Number where
+    (==) num1 num2 = fromEnum num1 == fromEnum num2
+instance Ord Number where
+    compare num1 num2 = compare (fromEnum num1) (fromEnum num2)
+
+
 -- Question 14.2
-data FiveSidedDie = S1 | S2 | S3 | S4 | S5 deriving (Show, Eq, Ord)
+data FiveSidedDie = S1 | S2 | S3 | S4 | S5 deriving (Enum, Eq, Show)
 
 class (Eq a, Enum a) => Die a where
     roll :: Int -> a
--- Ch14/type-classes.hs
+++ Ch14/type-classes.hs
@@ -8,16 +8,26 @@ instance Show SixSidedDie where
     show S5 = "five"
     show S6 = "six"
 
+-- Quick Check 14.1
+-- instance Show SixSidedDie where
+--     show S1 = "I"
+--     show S2 = "II"
+--     show S3 = "III"
+--     show S4 = "IV"
+--     show S5 = "V"
+--     show S6 = "VI"
+
 data Test1 = AA | ZZ deriving (Eq, Ord)
 data Test2 = ZZZ | AAA  deriving (Eq, Ord)
 
-data Name = Name (String, String) deriving (Show, Eq)
+newtype Name = Name (String, String) deriving (Show, Eq)
 
 names :: [Name]
-names = [ Name ("Emil","Cioran")
-        , Name ("Eugene","Thacker")
-        , Name ("Friedrich","Nietzsche") ]
-
+names =
+  [ Name ("Emil", "Cioran")
+  , Name ("Eugene", "Thacker")
+  , Name ("Friedrich", "Nietzsche")
+  ]
 
 instance Ord Name where
     compare (Name (f1, l1)) (Name (f2, l2)) = compare (l1, f1) (l2, f2)
