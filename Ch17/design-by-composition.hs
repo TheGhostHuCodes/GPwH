e9833bace2a827432312501af7eb36a7508de631
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2019-09-27 17:34:24 -0700

    Fix lint errors and add solutions for chapter 17 code

--- Ch17/design-by-composition.hs
+++ Ch17/design-by-composition.hs
@@ -1,5 +1,5 @@
-import Data.List
-import Data.Semigroup
+import           Data.List
+import           Data.Semigroup
 
 
 myLast :: [a] -> a
@@ -12,10 +12,11 @@ myMax :: Ord a => [a] -> a
 myMax = myLast . sort
 
 myAll :: (a -> Bool) -> [a] -> Bool
-myAll testFunc = (foldr (&&) True) . (map testFunc)
+myAll testFunc = foldr (&&) True . map testFunc
 
+-- Quick Check 17.1
 myAny :: (a -> Bool) -> [a] -> Bool
-myAny testFunc = (foldr (||) False) . (map testFunc)
+myAny testFunc = foldr (||) False . map testFunc
 
 instance Semigroup Integer where
     (<>) x y = x + y
@@ -26,9 +27,12 @@ data Color = Red
            | Green
            | Purple
            | Orange
-           | Brown deriving (Show, Eq)
+           | Brown
+           | Clear deriving (Show, Eq)
 
 instance Semigroup Color where
+    (<>) any Clear = any
+    (<>) Clear any = any
     (<>) Red Blue = Purple
     (<>) Blue Red = Purple
     (<>) Yellow Blue = Green
@@ -41,55 +45,75 @@ instance Semigroup Color where
              | all (`elem` [Red, Yellow, Orange]) [a, b] = Orange
              | otherwise = Brown
 
+-- Question 17.1
+instance Monoid Color where
+    mempty = Clear
+    mappend = (<>)
+
 howMuch :: Int -> String
-howMuch n | n > 10 = "a whole bunch"
-          | n > 0 = "not much"
+howMuch n | n > 10    = "a whole bunch"
+          | n > 0     = "not much"
           | otherwise = "we're in debt!"
 
-type Events = [String]
-type Probs = [Double]
+-- Question 17.2
+newtype Events = Events [String]
+instance Semigroup Events where
+    (<>) = combineEvents
+instance Monoid Events where
+    mempty = Events []
+    mappend = (<>)
+
+combineEvents :: Events -> Events -> Events
+combineEvents (Events e1) (Events e2) = Events (cartCombine combiner e1 e2)
+  where combiner x y = mconcat [x, "-", y]
+
+newtype Probs = Probs [Double]
+instance Semigroup Probs where
+    (<>) = combineProbs
+instance Monoid Probs where
+    mempty = Probs []
+    mappend = (<>)
+
+combineProbs :: Probs -> Probs -> Probs
+combineProbs (Probs p1) (Probs p2) = Probs (cartCombine (*) p1 p2)
 
 data PTable = PTable Events Probs
 
 instance Show PTable where
-    show (PTable events probs) = mconcat pairs
+    show (PTable (Events events) (Probs probs)) = mconcat pairs
         where pairs = zipWith showPair events probs
 
 instance Semigroup PTable where
-    (<>) ptable1 (PTable [] []) = ptable1
-    (<>) (PTable [] []) ptable2 = ptable2
+    (<>) ptable1 (PTable (Events []) (Probs [])) = ptable1
+    (<>) (PTable (Events []) (Probs [])) ptable2 = ptable2
     (<>) (PTable e1 p1) (PTable e2 p2) = createPTable newEvents newProbs
-        where newEvents = combineEvents e1 e2
-              newProbs = combineProbs p1 p2
+        where newEvents = e1 <> e2
+              newProbs = p1 <> p2
 
 instance Monoid PTable where
-    mempty = PTable [] []
+    mempty = PTable (Events []) (Probs [])
     mappend = (<>)
 
 createPTable :: Events -> Probs -> PTable
-createPTable events probs = PTable events normalizedProbs
-    where totalProbs = sum probs
-          normalizedProbs = map (\x -> x / totalProbs) probs
+createPTable events (Probs probs) = PTable events (Probs normalizedProbs)
+ where
+  totalProbs      = sum probs
+  normalizedProbs = map (\x -> x / totalProbs) probs
 
 showPair :: String -> Double -> String
 showPair event prob = mconcat [event, "|", show prob, "\n"]
 
-cartCombine :: (a ->  b -> c) -> [a] -> [b] -> [c]
+cartCombine :: (a -> b -> c) -> [a] -> [b] -> [c]
 cartCombine func l1 l2 = zipWith func newL1 cycledL2
-    where nToAdd = length l2
-          repeatedL1 = map (take nToAdd .repeat) l1
-          newL1 = mconcat repeatedL1
-          cycledL2 = cycle l2
-
-combineEvents :: Events -> Events -> Events
-combineEvents e1 e2 = cartCombine combiner e1 e2
-    where combiner = (\x y -> mconcat [x, "-", y])
-
-combineProbs :: Probs -> Probs -> Probs
-combineProbs p1 p2 = cartCombine (*) p1 p2
+ where
+  nToAdd     = length l2
+  repeatedL1 = map (take nToAdd . repeat) l1
+  newL1      = mconcat repeatedL1
+  cycledL2   = cycle l2
 
 coin :: PTable
-coin = createPTable ["heads", "tails"] [0.5, 0.5]
+coin = createPTable (Events ["heads", "tails"]) (Probs [0.5, 0.5])
 
 spinner :: PTable
-spinner = createPTable ["red", "blue", "green"] [0.1, 0.2, 0.7]
+spinner =
+  createPTable (Events ["red", "blue", "green"]) (Probs [0.1, 0.2, 0.7])
